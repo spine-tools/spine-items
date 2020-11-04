aa68f37afcbc90c199f3b978638ca8a5d8f94832
Antti Soininen <antti.soininen@vtt.fi>
2020-11-04 10:28:30 +0200

    Rewire Exporter's signals
    ProjectItem.open_directory() has not been annotated as @Slot which made
    disconnecting Exporter's signals fail at certain point. This reverts a
    previous fix which was just masking the underlying problem. We now call
    open_directory() from a lambda slot.

--- spine_items/exporter/exporter.py
+++ spine_items/exporter/exporter.py
@@ -137,8 +137,8 @@ class Exporter(ProjectItem):
     def make_signal_handler_dict(self):
         """Returns a dictionary of all shared signals and their handlers."""
         s = {
-            self._properties_ui.settings_button.clicked: lambda _: self.show_settings(),
-            self._properties_ui.open_directory_button.clicked: self.open_directory,
+            self._properties_ui.settings_button.clicked: self.show_settings,
+            self._properties_ui.open_directory_button.clicked: lambda _: self.open_directory(),
             self._properties_ui.cancel_on_error_check_box.stateChanged: self._cancel_on_error_option_changed,
         }
         return s
@@ -329,7 +329,8 @@ class Exporter(ProjectItem):
         if self._notifications.missing_settings:
             self.add_notification("Export settings missing.")
 
-    def show_settings(self):
+    @Slot(bool)
+    def show_settings(self, _=True):
         """Opens the item's settings window."""
         # Give window its own settings and indexing domains so Cancel doesn't change anything here.
         if self._settings_window is None:
