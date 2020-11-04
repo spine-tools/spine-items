5068debc72f9cbf60c1a4f6b10e69ad9f838d489
Antti Soininen <antti.soininen@vtt.fi>
2020-11-04 08:16:36 +0200

    Fix signal disconnect issue in Exporter
    No idea what went wrong here. When Exporter's icon was deactivated on
    Design view the second time (first time worked fine), Toolbox started to
    report that a signal failed to disconnect. The fix was to remove the
    @Slot decoration from the slot function although there was no indication
    whatsoever why this was a problem.

--- spine_items/exporter/exporter.py
+++ spine_items/exporter/exporter.py
@@ -137,7 +137,7 @@ class Exporter(ProjectItem):
     def make_signal_handler_dict(self):
         """Returns a dictionary of all shared signals and their handlers."""
         s = {
-            self._properties_ui.settings_button.clicked: self.show_settings,
+            self._properties_ui.settings_button.clicked: lambda _: self.show_settings(),
             self._properties_ui.open_directory_button.clicked: self.open_directory,
             self._properties_ui.cancel_on_error_check_box.stateChanged: self._cancel_on_error_option_changed,
         }
@@ -329,8 +329,7 @@ class Exporter(ProjectItem):
         if self._notifications.missing_settings:
             self.add_notification("Export settings missing.")
 
-    @Slot(bool)
-    def show_settings(self, _=True):
+    def show_settings(self):
         """Opens the item's settings window."""
         # Give window its own settings and indexing domains so Cancel doesn't change anything here.
         if self._settings_window is None:
