9067eaa7a2582a659627e2fd9beee75563e68ed8
Manuel Marin <manuelma@kth.se>
2020-11-12 20:51:19 +0100

    Add spine_items.utils

---
+++ spine_items/utils.py
@@ -0,0 +1,40 @@
+######################################################################################################################
+# Copyright (C) 2017-2020 Spine project consortium
+# This file is part of Spine Items.
+# Spine Items is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+# Public License for more details. You should have received a copy of the GNU Lesser General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+######################################################################################################################
+
+"""
+Contains utilities for all items.
+
+:authors: A. Soininen (VTT)
+:date:   1.4.2020
+"""
+
+
+def labelled_filepaths_from_resources(resources):
+    """Returns a dice mapping resource labels to filepaths available in given resources.
+    The label acts as an identifier for a 'transient_file'.
+    """
+    files = dict()
+    for resource in resources:
+        if resource.type_ == "file":
+            files[resource.path] = resource.path
+        elif resource.type_ == "transient_file":
+            files[resource.metadata["label"]] = resource.path
+    return files
+
+
+def make_label(label):
+    """
+    Annotates a label so the user can distinguish it from a normal path.
+
+    Args:
+        label (str): A label to annotate
+    """
+    return "{" + label + "}"
