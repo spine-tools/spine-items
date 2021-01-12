b2205c732c48a960c660794e74323f62c34c6968
Savolainen Pekka T <Pekka.t.Savolainen@vtt.fi>
2021-01-12 16:58:52 +0200

    Update required spine toolbox version

--- spine_items/version.py
+++ spine_items/version.py
@@ -9,6 +9,6 @@
 # this program. If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################################################
 __version__ = "0.4.9"
-REQUIRED_SPINE_TOOLBOX_VERSION = "0.6.23"
+REQUIRED_SPINE_TOOLBOX_VERSION = "0.5.24"
 REQUIRED_SPINE_ENGINE_VERSION = "0.9.7"
 REQUIRED_SPINEDB_API_VERSION = "0.10.0"
