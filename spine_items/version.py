d21166d4ce5ef09f4beddb658df97656f4db8d34
Manuel Marin <manuelma@kth.se>
2021-02-08 15:51:03 +0100

    Bump version to force an update
    Re Spine-project/Spine-Toolbox#1206

--- spine_items/version.py
+++ spine_items/version.py
@@ -8,7 +8,7 @@
 # Public License for more details. You should have received a copy of the GNU Lesser General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################################################
-__version__ = "0.4.23"
+__version__ = "0.4.24"
 REQUIRED_SPINE_TOOLBOX_VERSION = "0.5.29"
 REQUIRED_SPINE_ENGINE_VERSION = "0.9.15"
 REQUIRED_SPINEDB_API_VERSION = "0.10.8"
