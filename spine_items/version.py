dde2264f411842321eb5b892dfcf306ae9885777
Manuel Marin <manuelma@kth.se>
2021-03-14 18:10:11 +0100

    Bump versions
    Re Spine-project/Spine-Database-API#119

--- spine_items/version.py
+++ spine_items/version.py
@@ -8,7 +8,7 @@
 # Public License for more details. You should have received a copy of the GNU Lesser General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################################################
-__version__ = "0.4.66"
+__version__ = "0.5.0"
 REQUIRED_SPINE_TOOLBOX_VERSION = "0.5.39"
 REQUIRED_SPINE_ENGINE_VERSION = "0.9.25"
-REQUIRED_SPINEDB_API_VERSION = "0.11.17"
+REQUIRED_SPINEDB_API_VERSION = "0.11.18"
