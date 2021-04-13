66feba2e5864a9e934ed4822c5d3ea40c1caff3a
Pekka T Savolainen <pekka.t.savolainen@vtt.fi>
2021-04-13 14:30:14 +0300

    Bringing in category.py from toolbox
    spine_items should know the categories that are required to be on toolbox side. This helps in
    switching between different spine_items versions because we don't need to change anything
    on toolbox side.

--- spine_items/category.py
+++ spine_items/category.py
@@ -1,14 +1,30 @@
 ######################################################################################################################
 # Copyright (C) 2017-2021 Spine project consortium
 # This file is part of Spine Items.
-# Spine Items is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Spine Toolbox is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
 # Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
 # any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
 # Public License for more details. You should have received a copy of the GNU Lesser General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################################################
-__version__ = "0.6.36"
-REQUIRED_SPINE_TOOLBOX_VERSION = "0.5.43"
-REQUIRED_SPINE_ENGINE_VERSION = "0.9.29"
-REQUIRED_SPINEDB_API_VERSION = "0.11.48"
+
+"""
+This module defines the available project item categories.
+
+:author: A.Soininen (VTT)
+:date:   6.5.2020
+"""
+# The categories will appear in the main window in the same order they are declared here.
+CATEGORIES = ("Data Stores", "Data Connections", "Tools", "Views", "Importers", "Exporters", "Manipulators")
+
+
+CATEGORY_DESCRIPTIONS = {
+    "Data Connections": "Generic data source",
+    "Data Stores": "Data in the Spine generic format",
+    "Exporters": "Data conversion from Spine to an external format",
+    "Importers": "Data conversion from an external format to Spine",
+    "Tools": "Custom data processing",
+    "Views": "Data visualization",
+    "Manipulators": "Data conversion from Spine to Spine",
+}
