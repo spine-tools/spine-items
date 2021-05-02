a14b7ab50ae4a8fe2bcf5048a6c4ff2a249dedeb
Manuel Marin <manuelma@kth.se>
2021-05-02 14:28:48 +0200

    Add support for issuing commands interactively to a persistent
    Re Spine-project/spine-engine#36

--- spine_items/tool/tool.py
+++ spine_items/tool/tool.py
@@ -36,7 +36,7 @@ from .output_resources import scan_for_resources
 class Tool(ProjectItem):
 
     jupyter_console_requested = Signal(str, str, str)
-    persistent_console_requested = Signal(str, str, str, str)
+    persistent_console_requested = Signal(str, tuple, str, str)
     persistent_stdin_available = Signal(str, str)
     persistent_stdout_available = Signal(str, str)
     persistent_stderr_available = Signal(str, str)
@@ -507,29 +507,29 @@ class Tool(ProjectItem):
             self._filter_consoles[filter_id] = self._toolbox.make_jupyter_console(self, kernel_name, connection_file)
         self._project.toolbox().ui.listView_executions.model().layoutChanged.emit()
 
-    @Slot(str, str, str, str)
-    def _setup_persistent_console(self, filter_id, name, lexer_name, prompt):
+    @Slot(str, tuple, str, str)
+    def _setup_persistent_console(self, filter_id, key, lexer_name, prompt):
         """Sets up persistent console, eventually for a filter execution.
 
         Args:
             filter_id (str): filter identifier
-            name (str)
+            key (tuple)
             lexer_name (str)
             prompt (str)
         """
         if not filter_id:
-            self._setup_main_persistent_console(name, lexer_name, prompt)
+            self._setup_main_persistent_console(key, lexer_name, prompt)
         else:
-            self._setup_filter_persistent_console(filter_id, name, lexer_name, prompt)
+            self._setup_filter_persistent_console(filter_id, key, lexer_name, prompt)
 
-    def _setup_main_persistent_console(self, name, lexer_name, prompt):
+    def _setup_main_persistent_console(self, key, lexer_name, prompt):
         # pylint: disable=attribute-defined-outside-init
-        self.console = self._toolbox.make_persistent_console(self, name, lexer_name, prompt)
+        self.console = self._toolbox.make_persistent_console(self, key, lexer_name, prompt)
         self._project.toolbox().override_console()
 
-    def _setup_filter_persistent_console(self, filter_id, name, lexer_name, prompt):
+    def _setup_filter_persistent_console(self, filter_id, key, lexer_name, prompt):
         if filter_id not in self._filter_consoles:
-            self._filter_consoles[filter_id] = self._toolbox.make_persistent_console(self, name, lexer_name, prompt)
+            self._filter_consoles[filter_id] = self._toolbox.make_persistent_console(self, key, lexer_name, prompt)
         self._project.toolbox().ui.listView_executions.model().layoutChanged.emit()
 
     @Slot(str, str)
