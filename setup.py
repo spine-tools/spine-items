8b3dc1271644590fb8e97b214de6cfa6444d9394
Erkka Rinne <erkka.rinne@vtt.fi>
2021-06-28 11:13:10 +0300

    Upgrade to declarative package configuration

--- setup.py
+++ setup.py
@@ -16,47 +16,7 @@ Setup script for Python's setuptools.
 :date:   3.10.2019
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-from spine_items.version import (
-    __version__,
-    REQUIRED_SPINEDB_API_VERSION,
-    REQUIRED_SPINE_ENGINE_VERSION,
-    REQUIRED_SPINE_TOOLBOX_VERSION,
-)
-
-
-with open("README.md", encoding="utf8") as readme_file:
-    readme = readme_file.read()
-
-setup(
-    name="spine_items",
-    version=__version__,
-    description="Spine project items",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author="Spine Project consortium",
-    author_email="spine_info@vtt.fi",
-    url="https://github.com/Spine-project/spine-items",
-    packages=find_packages(exclude=("tests", "tests.*")),
-    install_requires=[
-        "spinedb_api >={}".format(REQUIRED_SPINEDB_API_VERSION),
-        "spine_engine >={}".format(REQUIRED_SPINE_ENGINE_VERSION),
-        "spinetoolbox >={}".format(REQUIRED_SPINE_TOOLBOX_VERSION),
-    ],
-    include_package_data=True,
-    license="LGPL-3.0-or-later",
-    zip_safe=False,
-    keywords="",
-    python_requires=">=3.6, <3.9",
-    test_suite="tests",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-        "Operating System :: OS Independent",
-    ],
-    project_urls={
-        "Issue Tracker": "https://github.com/Spine-project/Spine-Toolbox/issues",
-        "Documentation": "https://spine-toolbox.readthedocs.io/en/latest/project_items.html",
-    },
-)
+# NOTE: Package configuration in setup.cfg
+setup()
