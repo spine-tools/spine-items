2879cf7fc9cc38f939d1c6f8e1b39bfb10614b5a
Pekka T Savolainen <pekka.t.savolainen@vtt.fi>
2023-04-20 08:52:10 +0300

    Fix PySide6 tool filename case in build script and rebuild UI
    Re: spine-tools/Spine-Toolbox#2059

--- spine_items/data_store/ui/data_store_properties.py
+++ spine_items/data_store/ui/data_store_properties.py
@@ -13,7 +13,7 @@
 ################################################################################
 ## Form generated from reading UI file 'data_store_properties.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.4.1
+## Created by: Qt User Interface Compiler version 6.4.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
