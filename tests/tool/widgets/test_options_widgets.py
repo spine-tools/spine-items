3e9f4c2a29a79d63c1ad8b5788c888590ecec6fb
Pekka T Savolainen <pekka.t.savolainen@vtt.fi>
2023-09-29 17:17:08 +0300

    Add tests

---
+++ tests/tool/widgets/test_options_widgets.py
@@ -0,0 +1,49 @@
+######################################################################################################################
+# Copyright (C) 2017-2022 Spine project consortium
+# This file is part of Spine Items.
+# Spine Items is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+# Public License for more details. You should have received a copy of the GNU Lesser General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+######################################################################################################################
+
+"""Unit tests for the options_widgets.py module."""
+
+import unittest
+from unittest import mock
+import logging
+import sys
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtGui import QStandardItemModel, QStandardItem
+from spine_items.tool.widgets.options_widgets import JuliaOptionsWidget
+from spine_items.tool.tool_specifications import JuliaTool
+from tests.mock_helpers import create_mock_toolbox_with_mock_qsettings, MockQSettings
+
+
+class TestJuliaOptionsWidget(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Overridden method. Runs once before all tests in this class."""
+        try:
+            cls.app = QApplication().processEvents()
+        except RuntimeError:
+            pass
+        logging.basicConfig(
+            stream=sys.stderr,
+            level=logging.WARNING,
+            format="%(asctime)s %(levelname)s: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+
+    def test_options_widget(self):
+        ow = JuliaOptionsWidget()
+        ow.set_tool(QWidget())  # Obviously not a real Tool
+        ow._set_ui_at_work()
+        ow._set_ui_at_rest()
+        options = {"julia_sysimage": "/some/path"}
+        ow.do_update_options(options)
+        self.assertEqual("/some/path", ow.ui.lineEdit_sysimage.text())
